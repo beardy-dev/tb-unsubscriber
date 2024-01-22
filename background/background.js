5f1ce9db798d2857f63bfdeffd56bb1e709a88ac
beardy-dev <andrew@corsini.dev>
2024-01-21 22:47:28 -0600

    starting to mess around with background listeners

---
+++ background/background.js
@@ -0,0 +1,6 @@
+async function newMailListener(mailFolder, messageList) { 
+    console.log(mailFolder, messageList);
+}
+
+console.log('creating listener...');
+messenger.messages.onNewMailReceived.addListener(newMailListener);
