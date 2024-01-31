e858326697104ae27cf04fd4c2c59d0920763f3f
beardy-dev <andrew@corsini.dev>
2024-01-31 00:59:12 -0600

    trying to use vite and failing

--- background/background.js
+++
@@ -1,6 +0,0 @@
-async function newMailListener(mailFolder, messageList) { 
-    console.log(mailFolder, messageList);
-}
-
-console.log('creating listener...');
-messenger.messages.onNewMailReceived.addListener(newMailListener);
